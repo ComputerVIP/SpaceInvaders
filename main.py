6835f3b2fff6c3fe20c6457a22a83f79c6675cdf
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-11-03 11:48:25 -0700

    Brub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 pygame.init()
 
@@ -8,6 +9,29 @@ pygame.display.set_caption('Space Invaders')
 pygame_icon = pygame.image.load('resources\\ufo-1.png')
 #32x32
 pygame.display.set_icon(pygame_icon)
+
+
+class Bullet:
+    def __init__(self, x=0, y=0):
+        self.bullet_state = 'ready'
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load('resources\\bullet.png')
+        self.change = -2
+        self.rotate = pygame.transform.rotate(self.image, 90)
+
+    def shoot(self):
+        self.bullet_state = 'fire'
+        screen.blit(self.rotate, (self.x, self.y))
+
+    def move(self):
+        if self.bullet_state == 'fire':
+            self.y += self.change
+            screen.blit(self.rotate, (self.x, self.y))
+        if self.y <= 0:
+            self.bullet_state = 'ready'
+            self.y = 600 - 69
+
 
 class Player:
     def __init__(self, x, change = 0):
@@ -26,28 +50,43 @@ class Player:
         elif self.x >= 736:
             self.x = 736
 
+
+
+
 class Enemy:
     def __init__(self, x, y, change = 0):
         self.x = x
         self.y = y
         self.image = pygame.image.load('resources\\alien.png')
         self.change = change
-
-class Bullet:
-    def __init__(self, x, y, change_y = 1):
-        self.x = x
-        self.y = y
-        self.image = pygame.image.load('resources\\bullet.png')
-        self.change_y = change_y
+        self.x_change = 0.3
+        self.y_change = 20
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
-    
+
     def move(self):
-        self.y -= self.change_y
+        self.x += self.x_change
+        if self.x <= 0:
+            self.x_change = 1
+            self.y += self.y_change
+        elif self.x >= 736:
+            self.x_change = -1
+            self.y += self.y_change
+
+    def is_hit(self, bullet):
+        distance = (math.sqrt((self.x - bullet.x) ** 2 + (self.y - bullet.y) ** 2))
+        if distance < 27:
+            return True
+        return False
+
 
 player = Player(368)
+enemy = Enemy(random.randint(0, 736), random.randint(0, 236))
 bullet = Bullet(player.x + 16, player.y)
+
+
+
 
 
 
@@ -66,14 +105,19 @@ while running:
             if keys[pygame.K_RIGHT]:
                 player.change = 1
             if keys[pygame.K_SPACE]:
-                bullet.draw()
-                bullet.move()
+                if bullet.bullet_state == 'ready':
+                    bullet.x = player.x + 16
+                    bullet.y = player.y - 10
+                    bullet.shoot()
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player.change = 0
         
     player.move()
     player.draw()
+
+    enemy.draw()
+    enemy.move()
 
 
     bullet.move()
