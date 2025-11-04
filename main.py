a4e9f500c0ff657ea947393ca992ffcc804112bd
ComputerVIP <vincent.johnson@ucas-edu.net>
2025-11-04 11:49:13 -0700

    Made thing

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import pygame
 import random
 import math
+from pygame import mixer
 
 pygame.init()
+
+background = pygame.image.load('resources\\background-1.jpg')
+background = pygame.transform.scale(background, (800, 600))
+
+mixer.music.load('resources\\background.wav')
+mixer.music.play(-1)
+
+score_font = pygame.font.Font('freesansbold.ttf', 32)
+
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Space Invaders')
@@ -39,6 +49,8 @@ class Player:
         self.y = 600 - 69
         self.image = pygame.image.load('resources\\spaceship.png')
         self.change = change
+        self.score = 0
+        self.lives = 3
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
@@ -59,7 +71,7 @@ class Enemy:
         self.y = y
         self.image = pygame.image.load('resources\\alien.png')
         self.change = change
-        self.x_change = 0.3
+        self.x_change = 0.5 
         self.y_change = 20
 
     def draw(self):
@@ -75,26 +87,45 @@ class Enemy:
             self.y += self.y_change
 
     def is_hit(self, bullet):
-        distance = (math.sqrt((self.x - bullet.x) ** 2 + (self.y - bullet.y) ** 2))
-        if distance < 27:
+        distance = math.sqrt((self.x - bullet.x) ** 2 + (self.y - bullet.y) ** 2)
+        if distance < 48:
             return True
         return False
+    
+    def check_game_over(self):
+        if self.y > 540:
+            return True
+        return False
+    
+class GameOver:
+    def __init__(self):
+        pass
 
 
 player = Player(368)
 enemy = Enemy(random.randint(0, 736), random.randint(0, 236))
 bullet = Bullet(player.x + 16, player.y)
+enemies = []
+for i in range(6):
+    enemy_x = random.randint(0, 736)
+    enemy_y = random.randint(0, 236)
+    enemies.append(Enemy(enemy_x + i*10, enemy_y))
 
 
 
 
-
+game_over_font = pygame.font.Font('freesansbold.ttf', 64)
+game_over_txt = game_over_font.render('GAME OVER', True, (255, 0, 0))
+game_over = False
 
 
 
 running = True
 while running:
+    score_display = score_font.render(f'Score: {player.score}  Lives: {player.lives}', True, (255, 255, 255))
     screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    screen.blit(score_display, (10, 10))
     for event in pygame.event.get():
         keys = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
@@ -106,6 +137,7 @@ while running:
                 player.change = 1
             if keys[pygame.K_SPACE]:
                 if bullet.bullet_state == 'ready':
+                    mixer.Sound('resources\\laser.wav').play()
                     bullet.x = player.x + 16
                     bullet.y = player.y - 10
                     bullet.shoot()
@@ -115,11 +147,29 @@ while running:
         
     player.move()
     player.draw()
-
-    enemy.draw()
-    enemy.move()
-
+    for enemy in enemies:
+        if player.score >= 20:
+            enemy.y_change = 60
+        enemy.draw()
+        enemy.move()
+        if enemy.check_game_over():
+            player.lives -= 1
+            enemy.x = random.randint(0, 736)
+            enemy.y = random.randint(0, 236)
+            if player.lives <= 0:
+                game_over = True
+        elif enemy.is_hit(bullet) and bullet.bullet_state == 'fire':
+            player.score += 1
+            mixer.Sound('resources\\explosion.wav').play()
+            bullet.bullet_state = 'ready'
+            bullet.y = 600 - 69
+            enemy.x = random.randint(0, 736)
+            enemy.y = random.randint(0, 236)
+    if game_over:
+        screen.blit(game_over_txt, (200, 250))
+        enemies.clear()
 
     bullet.move()
+    
 
     pygame.display.flip()
